46c69d03a013b56741fca375b39ac96b3e22a3ab
Ozório <jleandro.dev@gmail.com>
2023-05-07 20:59:47 -0300

    Auth revomed

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -4,10 +4,16 @@ import { accessControllVerify } from "../middlewares/accessControllMidlleware";
 
 const eventRoutes = Router()
 
-eventRoutes.post('/', accessControllVerify(['admin']), eventCreate)
-eventRoutes.get('/:id', accessControllVerify(['admin', 'support', 'client']), eventShow)
-eventRoutes.get('/', accessControllVerify(['admin', 'support']), eventsShowAll)
-eventRoutes.put('/:id', accessControllVerify(['admin']), eventUpdate)
-eventRoutes.delete('/:id', accessControllVerify(['admin']), eventRemove)
+// eventRoutes.post('/', accessControllVerify(['admin']), eventCreate)
+// eventRoutes.get('/:id', accessControllVerify(['admin', 'support', 'client']), eventShow)
+// eventRoutes.get('/', accessControllVerify(['admin', 'support']), eventsShowAll)
+// eventRoutes.put('/:id', accessControllVerify(['admin']), eventUpdate)
+// eventRoutes.delete('/:id', accessControllVerify(['admin']), eventRemove)
+
+eventRoutes.post('/', eventCreate)
+eventRoutes.get('/:id', eventShow)
+eventRoutes.get('/', eventsShowAll)
+eventRoutes.put('/:id', eventUpdate)
+eventRoutes.delete('/:id', eventRemove)
 
 export { eventRoutes }
